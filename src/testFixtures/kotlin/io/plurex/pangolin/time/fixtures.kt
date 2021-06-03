5d633aeccb51eff232ed173dce3ec64b36e57d8e
laurence <lgwillmore@gmail.com>
2021-06-03 11:03:32 +0200

    Add time mocking cleanup helper

--- src/testFixtures/kotlin/io/plurex/pangolin/time/fixtures.kt
+++ src/testFixtures/kotlin/io/plurex/pangolin/time/fixtures.kt
@@ -2,13 +2,12 @@ package io.plurex.pangolin.time
 
 import io.mockk.every
 import io.mockk.mockk
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import java.time.*
 
 /**
  * Sets the current time to the time provided, or freezes it at current time if time is not provided.
+ *
+ * Be sure to use [itIsNowRealTime] after testing to clean up.
  */
 fun itIsNow(now: ZonedDateTime = LocalDateTime.now().atZone(ZoneId.of("UTC"))): ZonedDateTime {
     clock = mockk {
@@ -19,10 +18,19 @@ fun itIsNow(now: ZonedDateTime = LocalDateTime.now().atZone(ZoneId.of("UTC"))): 
 
 /**
  * Freezes time at the current time and returns the Unix epoch millis.
+ *
+ * Be sure to use [itIsNowRealTime] after testing to clean up.
  */
 fun itIsNowMillis(nowMillis: Long = timeNowMillis()): Long {
     clock = mockk {
         every { instant() } returns Instant.ofEpochMilli(nowMillis)
     }
     return nowMillis
+}
+
+/**
+ * Puts time back to reality
+ */
+fun itIsNowRealTime() {
+    clock = Clock.systemUTC()
 }
