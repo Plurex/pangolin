78aa5bf82dbd691e1efbd8e489e3fb9de6c38476
laurence <lgwillmore@gmail.com>
2022-07-27 12:13:04 +0100

    fix aes tests

--- src/test/kotlin/io/plurex/pangolin/secrets/SecretsAESTest.kt
+++ src/test/kotlin/io/plurex/pangolin/secrets/SecretsAESTest.kt
@@ -11,19 +11,19 @@ internal class SecretsAESTest {
 
     @Test
     fun `encrypt - decrypt`() {
-        encryptDecryptHelper(EncryptionAlgo.AesCbcPadding)
-        encryptDecryptHelper(EncryptionAlgo.Aes)
+        encryptDecryptHelper(EncryptionAlgo.AesCbcPadding, KeySize.BITS_128, KeySize.BITS_192, KeySize.BITS_256)
+        encryptDecryptHelper(EncryptionAlgo.Aes, KeySize.BITS_128, KeySize.BITS_192, KeySize.BITS_256)
     }
 
     @Test
     fun `encrypt - decrypt - bytearray`() {
-        encryptDecryptByteArrayHelper(EncryptionAlgo.AesCbcPadding)
-        encryptDecryptByteArrayHelper(EncryptionAlgo.Aes)
+        encryptDecryptByteArrayHelper(EncryptionAlgo.AesCbcPadding, KeySize.BITS_128, KeySize.BITS_192, KeySize.BITS_256)
+        encryptDecryptByteArrayHelper(EncryptionAlgo.Aes, KeySize.BITS_128, KeySize.BITS_192, KeySize.BITS_256)
     }
 
-    private fun encryptDecryptHelper(algo: EncryptionAlgo) {
+    private fun encryptDecryptHelper(algo: EncryptionAlgo, vararg keySizeOptions: KeySize) {
         for (i in 0..20) {
-            val key = generateSecretKey(randEnum())
+            val key = generateSecretKey(keySizeOptions.random())
             val testObj = SecretsAES(key, algo)
             val original = randText()
             val encrypted = testObj.encrypt(original)
@@ -36,9 +36,9 @@ internal class SecretsAESTest {
         }
     }
 
-    private fun encryptDecryptByteArrayHelper(algo: EncryptionAlgo) {
+    private fun encryptDecryptByteArrayHelper(algo: EncryptionAlgo, vararg keySizeOptions: KeySize) {
         for (i in 0..20) {
-            val key = generateSecretKey(randEnum())
+            val key = generateSecretKey(keySizeOptions.random())
             val testObj = SecretsAES(key, algo)
             val original = randText().toByteArray()
             val encrypted = testObj.encrypt(original)
